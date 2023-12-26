b1e9e009236f384943e17fbceb5f4f4913b8fbc2
Aurosish Rout <kishanaurosish@gmail.com>
2023-12-26 19:51:15 +0530

    committed

--- server.js
+++ server.js
@@ -3,57 +3,65 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import axios from "axios"
+import OpenAI from "openai";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 5500;
 
-app.use(bodyParser.urlencoded({extended:1}));
+app.use(bodyParser.urlencoded({ extended: 1 }));
 
 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/public/index.html");
-})
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-// app.post("/submit" , (req , res) => {
-//     console.log(req.body);
-//     res.render("index.ejs");
-// })
 
+
+
+
+const openai = new OpenAI({ apiKey: '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' });
 
 app.post("/submit", async (req, res) => {
-    try {
-      const response = await axios.post(
-        'https://api.openai.com/v1/engines/davinci-codex/completions',
-        {
-          prompt: `Generate multiplechoise questions on ${req.body.btnradio} with ${req.body.btnradio2} level`, // Use the question from the request body
-          max_tokens: 300,
-          temperature: 0.7,
-        },
-        {
-          headers: {
-            'Authorization': `Bearer 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`, // Replace with your actual API key
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-  
-  
-      const generatedQuiz = response.data.choices[0].text.trim();
-      console.log(generatedQuiz);
-      res.render(index.ejs , {question:generatedQuiz});
-      // ...
-    } catch (error) {
-      console.error(error);
+
+  let btnradio = req.body.btnradio;
+  let btnradio1 = req.body.btnradio1;
+  let btnradio2 = req.body.btnradio2;
+
+  const prompt = "Ask me a question on" + btnradio + " which is a " + btnradio1 + " question with " + btnradio2 + " level ";
+  try {
+
+    async function main() {
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: prompt }],
+        model: "gpt-3.5-turbo",
+      });
+
+
+      const resp = completion.choices[0];
+      const responseWithOptions = resp.message.content.replace("[Option 1]", "Your Option 1")
+        .replace("[Option 2]", "Your Option 2")
+        .replace("[Option 3]", "Your Option 3");
+
+        
+      res.render("index.ejs", { content:responseWithOptions});
     }
+
+    main();
+
+  } catch (error) {
+    console.error(error.messages);
+  }
+
+
 });
 
 
 
 app.listen(port, () => {
-    console.log("The server is runnning on the port", port);
+  console.log("The server is runnning on the port", port);
 })
 
 
