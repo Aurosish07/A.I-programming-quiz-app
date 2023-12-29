4f0cfad55269e1eb389a1ae0edb7cd9d21e3fcc4
Aurosish Rout <kishanaurosish@gmail.com>
2023-12-29 19:34:44 +0530

    committed

--- server.js
+++ server.js
@@ -30,27 +30,27 @@ app.post("/submit", async (req, res) => {
   let btnradio1 = req.body.btnradio1;
   let btnradio2 = req.body.btnradio2;
 
-  const prompt = "Ask me a question on" + btnradio + " which is a " + btnradio1 + " question with " + btnradio2 + " level ";
+  const prompt = "Ask me a question on" + btnradio + " which is a " + btnradio1 + " question with " + btnradio2 + " level with answer";
   try {
 
-    async function main() {
-      const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: prompt }],
-        model: "gpt-3.5-turbo",
-      });
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant designed to output JSON.",
+        },
+        { role: "user", content: prompt },
+      ],
+      model: "gpt-3.5-turbo-1106",
+      response_format: { type: "json_object" },
+    });
 
+    let result = completion.choices[0].message.content;
+    let obj = JSON.parse(result);
 
-      const resp = completion.choices[0];
-      const responseWithOptions = resp.message.content.replace("[Option 1]", "Your Option 1")
-        .replace("[Option 2]", "Your Option 2")
-        .replace("[Option 3]", "Your Option 3");
+    res.render("index.ejs" , {question:obj.question , option1:obj.options[0], option2:obj.options[1] , option3:obj.options[2] , option4:obj.options[3] , ans:obj.answer})
+   console.log(obj)
 
-        
-      // res.render("index.ejs", { content:responseWithOptions});
-      console.log(responseWithOptions)
-    }
-
-    main();
 
   } catch (error) {
     console.error(error.messages);
